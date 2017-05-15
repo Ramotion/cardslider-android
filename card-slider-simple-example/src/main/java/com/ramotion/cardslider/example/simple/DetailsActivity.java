b5eb5a1ab4863f16ecfcaa2e30c30b284b796682
dvg4000 <dvgrishechkin@yandex.ru>
2017-05-15 17:51:46 +0300

    Improve image loading in DetailsActivity

--- card-slider-simple-example/src/main/java/com/ramotion/cardslider/example/simple/DetailsActivity.java
+++ card-slider-simple-example/src/main/java/com/ramotion/cardslider/example/simple/DetailsActivity.java
@@ -1,8 +1,11 @@
 package com.ramotion.cardslider.example.simple;
 
 import android.graphics.Bitmap;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.transition.Transition;
 import android.util.DisplayMetrics;
 import android.widget.ImageView;
 
@@ -10,6 +13,7 @@ public class DetailsActivity extends AppCompatActivity {
 
     static String BUNDLE_IMAGE_ID = "BUNDLE_IMAGE_ID";
 
+    private ImageView imageView;
     private DecodeBitmapTask decodeBitmapTask;
 
     @Override
@@ -23,8 +27,37 @@ public class DetailsActivity extends AppCompatActivity {
             return;
         }
 
-        ((ImageView)findViewById(R.id.image_view)).setImageResource(smallResId);
+        imageView = (ImageView)findViewById(R.id.image);
+        imageView.setImageResource(smallResId);
 
+        if (Build.VERSION.SDK_INT < 21) {
+            loadFullSizeBitmap(smallResId);
+        } else {
+            getWindow().getSharedElementEnterTransition().addListener(new Transition.TransitionListener() {
+                @Override public void onTransitionStart(Transition transition) {
+                    boolean isClosing = decodeBitmapTask != null;
+                    if (isClosing) {
+                        ((CardView)findViewById(R.id.card)).setRadius(15f);
+                    }
+                }
+                @Override public void onTransitionPause(Transition transition) {}
+                @Override public void onTransitionResume(Transition transition) {}
+                @Override public void onTransitionEnd(Transition transition) { loadFullSizeBitmap(smallResId);}
+                @Override public void onTransitionCancel(Transition transition) { loadFullSizeBitmap(smallResId);}
+            });
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (isFinishing() && decodeBitmapTask != null) {
+            decodeBitmapTask.cancel(true);
+        }
+    }
+
+    private void loadFullSizeBitmap(int smallResId) {
         int bigResId;
         switch (smallResId) {
             case R.drawable.p1: bigResId = R.drawable.p1_big; break;
@@ -42,18 +75,10 @@ public class DetailsActivity extends AppCompatActivity {
             @Override
             protected void onPostExecute(Bitmap bitmap) {
                 super.onPostExecute(bitmap);
-                ((ImageView)findViewById(R.id.image_view)).setImageBitmap(bitmap);
+                imageView.setImageBitmap(bitmap);
             }
         };
         decodeBitmapTask.execute();
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-        if (isFinishing() && decodeBitmapTask != null) {
-            decodeBitmapTask.cancel(true);
-        }
-    }
 }
