78d7fe8f6483e4167b5b0713b08875b06f0d9745
dvg4000 <dvgrishechkin@yandex.ru>
2017-09-07 12:59:55 +0300

    #9 Fix not smooth scrolling, with big cardsGap
     Based on #10 bug fix

--- card-slider/src/main/java/com/ramotion/cardslider/DefaultViewUpdater.java
+++ card-slider/src/main/java/com/ramotion/cardslider/DefaultViewUpdater.java
@@ -79,7 +79,11 @@ public class DefaultViewUpdater implements CardSliderLayoutManager.ViewUpdater {
             scale = SCALE_CENTER - SCALE_CENTER_TO_RIGHT * ratio;
             alpha = 1;
             z = Z_CENTER_2;
-            x = -Math.min(transitionRight2Center, transitionRight2Center * (viewLeft - transitionEnd) / transitionDistance);
+            if (Math.abs(transitionRight2Center) < Math.abs(transitionRight2Center * (viewLeft - transitionEnd) / transitionDistance)) {
+                x = -transitionRight2Center;
+            } else {
+                x = -transitionRight2Center * (viewLeft - transitionEnd) / transitionDistance;
+            }
         } else {
             scale = SCALE_RIGHT;
             alpha = 1;
